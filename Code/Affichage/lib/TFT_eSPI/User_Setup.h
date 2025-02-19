3326322a5e5a64bc2077e7c1f62558ad539dda7f
MASelectronique <97920078+MASelectronique@users.noreply.github.com>
2025-02-19 08:43:04 -0500

    test

--- Code/Affichage/lib/TFT_eSPI/User_Setup.h
+++ Code/Affichage/lib/TFT_eSPI/User_Setup.h
@@ -42,8 +42,8 @@
 //#define RPI_DISPLAY_TYPE // 20MHz maximum SPI
 
 // Only define one driver, the other ones must be commented out
-#define ILI9341_DRIVER       // Generic driver for common displays
-//#define ILI9341_2_DRIVER     // Alternative ILI9341 driver, see https://github.com/Bodmer/TFT_eSPI/issues/1172
+//#define ILI9341_DRIVER       // Generic driver for common displays
+#define ILI9341_2_DRIVER     // Alternative ILI9341 driver, see https://github.com/Bodmer/TFT_eSPI/issues/1172
 //#define ST7735_DRIVER      // Define additional parameters below for this display
 //#define ILI9163_DRIVER     // Define additional parameters below for this display
 //#define S6D02A1_DRIVER
@@ -167,13 +167,13 @@
 // ###### EDIT THE PIN NUMBERS IN THE LINES FOLLOWING TO SUIT YOUR ESP8266 SETUP ######
 
 // For NodeMCU - use pin numbers in the form PIN_Dx where Dx is the NodeMCU pin designation
-#define TFT_MISO  PIN_D6  // Automatically assigned with ESP8266 if not defined
+/*#define TFT_MISO  PIN_D6  // Automatically assigned with ESP8266 if not defined
 #define TFT_MOSI  PIN_D7  // Automatically assigned with ESP8266 if not defined
 #define TFT_SCLK  PIN_D5  // Automatically assigned with ESP8266 if not defined
 
 #define TFT_CS    PIN_D8  // Chip select control pin D8
 #define TFT_DC    PIN_D3  // Data Command control pin
-#define TFT_RST   PIN_D4  // Reset pin (could connect to NodeMCU RST, see next line)
+#define TFT_RST   PIN_D4  // Reset pin (could connect to NodeMCU RST, see next line)*/
 //#define TFT_RST  -1     // Set TFT_RST to -1 if the display RESET is connected to NodeMCU RST or 3.3V
 
 
@@ -209,12 +209,12 @@
 // For ESP32 Dev board (only tested with ILI9341 display)
 // The hardware SPI can be mapped to any pins
 
-//#define TFT_MISO 19
-//#define TFT_MOSI 23
-//#define TFT_SCLK 18
-//#define TFT_CS   15  // Chip select control pin
-//#define TFT_DC    2  // Data Command control pin
-//#define TFT_RST   4  // Reset pin (could connect to RST pin)
+#define TFT_MISO 3
+#define TFT_MOSI 1
+#define TFT_SCLK 2
+#define TFT_CS   0 // Chip select control pin
+#define TFT_DC   10  // Data Command control pin
+#define TFT_RST  4  // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
 // For ESP32 Dev board (only tested with GC9A01 display)
@@ -227,7 +227,7 @@
 //#define TFT_RST  33  // Reset pin (could connect to Arduino RESET pin)
 //#define TFT_BL   22  // LED back-light
 
-//#define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
+#define TOUCH_CS 8     // Chip select pin (T_CS) of touch screen
 
 //#define TFT_WR 22    // Write strobe for modified Raspberry Pi TFT only
 
@@ -357,11 +357,11 @@
 // With an ILI9163 display 27 MHz works OK.
 
 // #define SPI_FREQUENCY   1000000
-// #define SPI_FREQUENCY   5000000
+ //#define SPI_FREQUENCY   5000000
 // #define SPI_FREQUENCY  10000000
 // #define SPI_FREQUENCY  20000000
-#define SPI_FREQUENCY  27000000
-// #define SPI_FREQUENCY  40000000
+//#define SPI_FREQUENCY  27000000
+ #define SPI_FREQUENCY  40000000
 // #define SPI_FREQUENCY  55000000 // STM32 SPI1 only (SPI2 maximum is 27MHz)
 // #define SPI_FREQUENCY  80000000
 
--- Code/Affichage/src/main.cpp
+++ Code/Affichage/src/main.cpp
@@ -1,105 +1,154 @@
+/* Correction: 17/20
+Wire.write(0x3B); utiliser des constantes pour les registres
+int16_t AcX,AcY,AcZ;	mettre un g_ devant. Est-ce obligatoire d'avoir ces variables globales? Sinon, mettre locales.
+ 
+ Expliquer d'où viennent ces valeurs.
+ //Valeurs d'offset pour X,Y,Z  
+  AcXoff = -950;
+  AcYoff = -300;
+  AcZoff = 0;
+
+if (g_pitch >= 30 || g_pitch <= -30)  Utiliser des constantes
+
+Faire des fonctions dans la boucle principales.
+*/
+
+
+/**
+  @file   main.cpp, projet: Prototype affichage Trottinette électrique
+  @author Lucas Lalumière Longpré
+  @date   7 Octobre 2024
+  @brief  Code pour le prototype du système d'affichage du
+		      projet Trottinette électrique. Communique par I2C 
+          avec un accéléromètre MPU-6050, à partir des valeurs
+          X,Y,Z de l'accéléromètre, on calcule les valeurs de roulis
+          et de tangage, ces valeurs sont utile pour la detection de 
+          chute de la trottinette. Les deux valeurs sont affichées
+          à l'écran et au terminal série. L'écran est tactile et affiche
+          un message lorsqu'on appuie dessus. La DEL du MCU s'allume pour
+          indiquer que la trottinette est tombée.
+
+  Environnement:
+      Développement: Visual Studio 2022
+  Matériel: -MCU = ESP32-C3 DevkitC-02
+            -Écran = ILI9341
+            -Accéléromètre = MPU-6050
+*/
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <math.h>
+#include <TFT_eSPI.h>      //Librairie pour l'écran tactile
 #include <lvgl.h>
-#include <TFT_eSPI.h>
 #include <ui.h>
-#include "TouchScreen.h"
 
-// TODO: Re-Define according to actual wiring of touch display
-#define YP A1
-#define XM A0
-#define YM 4
-#define XP 5
+//Définition des paramètres pour l'écran
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+uint16_t calData[5] = { 610, 2638, 592, 3103, 4 };  //Valeurs de calibration de l'écran
 
-// TODO: Replace with your screen resolution
-static const uint16_t screenWidth  = 240;
-static const uint16_t screenHeight = 320;
+//Définition des pattes pour communication I2C
+#define SDA_PIN 18  
+#define SCL_PIN 19 
 
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[ screenWidth * screenHeight / 10 ];
+//Définition des paramètres pour l'accéléromètre
+const uint8_t MPU=0x68; //Adresse de l'accéléromètre MPU 6050
+int16_t AcX,AcY,AcZ;	//Valeurs X,Y,Z de l'accéléromètre
+double g_pitch,g_roll;	//Variables de roulis (roll) et de tangage (pitch)
 
-bool touched = false;
-bool coordinatesSet = false;
+TFT_eSPI tft = TFT_eSPI(); //Déclare un objet pour l'écran tactile
 
-// TODO: Replace with your screen's touch controller
-TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight);
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+void getAngle(int Vx,int Vy,int Vz);  //Fonction qui calcule le roulis et le tangage de l'accéléromètre
 
-void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
-{
-    uint32_t w = ( area->x2 - area->x1 + 1 );
-    uint32_t h = ( area->y2 - area->y1 + 1 );
+void setup() {
+  Serial.begin(115200);
 
-    tft.startWrite();
-    tft.setAddrWindow(area->x1, area->y1, w, h);
-    tft.pushColors((uint16_t *)&color_p->full, w * h, true);
-    tft.endWrite();
+  tft.init();	//Initialise l'écran tactile
+  tft.setRotation(0); // 0 = (0,0) en haut à gauche/ 1 = (0,0) est bas à gauche/ 2 = (0,0) est haut à droite / 3 = en bas à droite
+  //Vide l'écran et met la couleur de la police à noir
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  tft.setTouch(calData);  //Calibre l'écran avec les valeurs de calibration
 
-    lv_disp_flush_ready(disp);
+  pinMode(LED_BUILTIN, OUTPUT);	//Initialise la DEL du MCU comme une sortie
+  Wire.begin(SDA_PIN, SCL_PIN);	//Initialise la communication I2C
+  Wire.beginTransmission(MPU);	//Commence la communication avec l'accéléromètre
+  Wire.write(0x6B);   //Écrit au registre 0x6B PWR_MGMT1 (Power management)
+  Wire.write(0);      //Met tous les bits de ce registre à 0, ce qui désactive le mode "sleep"
+  Wire.endTransmission(true); //Arrête la transmission
 }
 
-void my_touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data)
-{
-  // TODO: Replace with your custom touch-screen handling logic!
+void loop(){
+  uint16_t t_x = 0, t_y = 0; //Variables pour storer les coordonnées du "touch"
+  int AcXoff,AcYoff,AcZoff; //Variables d'offset pour X,Y,Z
 
-  TSPoint p = ts.getPoint();
-  touched = (p.z > 375 && p.z < 415);
+  digitalWrite(LED_BUILTIN, LOW);	//Met la DEL à un niveau bas
 
-  float x = 1.0f * p.x - 200.0f;
-  float y = 1.0f * p.y - 135.0f;
-  x = (x / 685.0f) * 240.0f;
-  y = (y / 735.0f) * 320.0f;
+  Wire.beginTransmission(MPU);  //Recommence la transmission avec l'accéléromètre
+  Wire.write(0x3B); //Adresse le registre 0x3B (ACCEL_XOUT)
+  Wire.endTransmission(false);  //Indique qu'on veut continuer la transmission
+  Wire.requestFrom(MPU,(size_t)6,(bool)true);  //Fait une requête de données de 6 octets à l'accéléromètre en "repeated start"
 
-  x = (x - 480.0f) * -1.0f - 240;
-  y = (y - 640.0f) * -1.0f - 320;
+  //Valeurs d'offset pour X,Y,Z
+  AcXoff = -950;
+  AcYoff = -300;
+  AcZoff = 0;
 
-  if(touched && !coordinatesSet)
-  {
-    data->state = LV_INDEV_STATE_PR;
+  //Lit les données de l'accéléromètre
+  AcX=(Wire.read()<<8|Wire.read()) + AcXoff;  //Combine les 8 bits LSB et les 8 bits MSB + le offset
+  AcY=(Wire.read()<<8|Wire.read()) + AcYoff;
+  AcZ=(Wire.read()<<8|Wire.read()) + AcZoff;
 
-    
-    data->point.x = x;
-    data->point.y = y;
+  //Calcule le roulis et tangage
+  getAngle(AcX,AcY,AcZ);
 
-    coordinatesSet = true; 
+  //Affiche les valeurs de roulis et de tangage au terminal série
+  Serial.print("Angle: ");
+  Serial.print("Pitch = "); Serial.print(g_pitch);
+  Serial.print(" | Roll = "); Serial.println(g_roll);
+  //Met la grosseur du tecte à 2, la couleur du texte est noire
+  tft.setTextColor(TFT_BLACK);
+  tft.setCursor(0,80);
+  tft.setTextSize(2);
+  //Affiche les valeurs de roulis et de tangage à l'écran
+  tft.println("Angle: ");
+  tft.print("Pitch = "); tft.println(g_pitch);
+  tft.print("Roll = "); tft.println(g_roll);
+
+  bool pressed = tft.getTouch(&t_x, &t_y);  //Vérifie si l'écran a été touché
+
+  if (pressed == true) {  //Si l'utilisateur touche l'écran
+    tft.setCursor(15,150);
+    tft.print("SCREEN TOUCHED");  //Affiche un message à l'écran
   }
-  
-  if (!touched)
-  {
-    data->state = LV_INDEV_STATE_REL;
-    coordinatesSet = false;
+
+  if (g_pitch >= 30 || g_pitch <= -30)  { //Si le tangage est en haut de 30 ou plus bas que -30 (Trottinette tombée sur le côté)
+    digitalWrite(LED_BUILTIN, HIGH);	//Allume la DEL du MCU
   }
+  else if (g_roll >= 30 || g_roll <= -30) { //Si le roulis est en haut de 30 ou plus bas que -30 (Trottinette tombée vers l'avant ou vers l'arrière)
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+
+  delay(500);
+  tft.fillScreen(TFT_WHITE);  //Vide l'écran
 }
 
-void setup()
-{
-  lv_init();
 
-  tft.begin();
-  tft.setRotation(2);
+/*Brief: Fonction pour convertir les valeurs x, y et z 
+  provenant de l'accéléromètre en valeurs de roulis et 
+  de tangage. Utile pour détecter si la trottinette est
+  tombée sur le côté.
+  Param: int Vx, Vy, Vz
+  Return: Aucun
+  */
+void getAngle(int Vx,int Vy,int Vz) {
+  double x = Vx;
+  double y = Vy;
+  double z = Vz;
 
-  lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * screenHeight / 10);
-
-  // Register the custom display function
-  static lv_disp_drv_t disp_drv;
-  lv_disp_drv_init( &disp_drv );
-  disp_drv.hor_res = screenWidth;
-  disp_drv.ver_res = screenHeight;
-  disp_drv.flush_cb = my_disp_flush;
-  disp_drv.draw_buf = &draw_buf;
-  lv_disp_drv_register( &disp_drv );
-
-  // Register the custom touch-input handler
-  static lv_indev_drv_t indev_drv;
-  lv_indev_drv_init( &indev_drv );
-  indev_drv.type = LV_INDEV_TYPE_POINTER;
-  indev_drv.read_cb = my_touchpad_read;
-  lv_indev_drv_register( &indev_drv );
-
-  // Init EEZ-Studio UI
-  ui_init();
-}
-
-void loop() {
-  lv_timer_handler();
-  // Update EEZ-Studio UI
-  ui_tick();
+  g_pitch = atan(x/sqrt((y*y) + (z*z)));  //Calcul du tangage en radian
+  g_roll = atan(y/sqrt((x*x) + (z*z)));   //Calcul du roulis en radian
+  //Convertie les valeurs en degré
+  g_pitch = g_pitch * (180.0/3.14);
+  g_roll = g_roll * (180.0/3.14);
 }
