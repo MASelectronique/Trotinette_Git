0831518d72022d2d87f6a5f82d63eb664c71019f
LucasLalumiereLongpre <lucaslalumierelongpre@gmail.com>
2025-04-07 13:58:42 -0400

    Mise a jour
    Rev2 PCB et Code telemetrie

--- Code/Telemetrie/.pio/build/esp32-s3-devkitc-1/FrameworkArduino/main.cpp.d
+++ Code/Telemetrie/.pio/build/esp32-s3-devkitc-1/FrameworkArduino/main.cpp.d
@@ -2,7 +2,7 @@
  C:/Users/yuyam/.platformio/packages/framework-arduinoespressif32/cores/esp32/main.cpp \
  C:/Users/yuyam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/freertos/include/freertos/FreeRTOS.h \
  C:/Users/yuyam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/freertos/include/esp_additions/freertos/FreeRTOSConfig.h \
- C:/Users/yuyam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/qio_qspi/include/sdkconfig.h \
+ C:/Users/yuyam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/qio_opi/include/sdkconfig.h \
  C:/Users/yuyam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_compiler.h \
  C:/Users/yuyam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/freertos/port/xtensa/include/freertos/FreeRTOSConfig_arch.h \
  C:/Users/yuyam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/freertos/port/xtensa/include/freertos/xtensa_config.h \
--- Code/Telemetrie/src/main.cpp
+++ Code/Telemetrie/src/main.cpp
@@ -10,15 +10,26 @@
             -Accéléromètre = ICM-42670-P
 */
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include <ArduinoJson.h>  //Lib pour JSON
+#include <Wire.h>
 #include <ICM42670P.h>    //Lib pour accéléromètre
+#include <math.h>
 
-#define RX_PIN 36
-#define TX_PIN 37
+#define RX_PIN 18
+#define TX_PIN 17
+
+#define SDA_PIN 8
+#define SCL_PIN 9
+#define INT_PIN 5
+
+#define LED_PIN 2
 
 JsonDocument doc;   //Objet JSON reçu
 String jsonString;  //Objet JSON décodé
+String jsonErrorMessage;  //Json a affiché si erreur de décodage
 DeserializationError jsonError;  //Variable qui retourne le résultat du décodage du JSON
+EspSoftwareSerial::UART jsonSerial; //Interface UART pour recevoir JSON
 
 struct Ctrl { //Struct qui contient les données du contrôleur
   int etat;
@@ -41,20 +52,69 @@ struct Data { //Struct qui contient les données de la trottinette
   Drive drive;
 };
 
-void printData(const Data &t);
+void printData(const Data &t);  //Fonction pour afficher les données de la trottinette
 
-Data trottinette;
+Data trottinette; //L'objet trottinette contenant tous les données de la trottinette
+
+ICM42670 IMU(Wire,0); //Crée l'objet de l'accelerateur en I2C
+float x,y,z = 0.0;
+int pitch = 0;
+int pitch_offset = -41;
 
 void setup() {
-  Serial.begin(115200); //Moniteur série à 115200 bauds
-  Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);  //Interface UART1 à 115200 bauds
+  int ret;
+  Serial.begin(115200); //Port pour moniteur série
+  jsonSerial.begin(9600, EspSoftwareSerial::SWSERIAL_8N1, RX_PIN, TX_PIN);  //Port pour recevoir objet JSON
+  while(!Serial) {} //Attend que le moniteur série soit initialisé
+
+  pinMode(INT_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+
+  Wire.begin(SDA_PIN, SCL_PIN); //Initialise l'interface I2C
+  //Initialise l'accelerateur ICM42670P
+  ret = IMU.begin();
+  if (ret != 0) {
+    Serial.print("ICM42670 initialization failed: ");
+    Serial.println(ret);
+    while(1); //Si erreur d'init, on bloque le programme
+  }
+
+  // Accel ODR = 100 Hz and Full Scale Range = 16G
+  IMU.startAccel(100,16);
 }
 
 void loop() {
-  if (Serial1.available()) {
-    jsonString = Serial1.readString();  //Lit objet reçu par UART
-    jsonError = deserializeJson(doc, jsonString);  //Décode l'objet
+  inv_imu_sensor_event_t imu_event;
+
+  // Get last event
+  IMU.getDataFromRegisters(imu_event);
+  
+  x = imu_event.accel[0];
+  y = imu_event.accel[1];
+  z = imu_event.accel[2];
+
+  // Calculate pitch and roll
+  pitch = atan2(y, sqrt(x * x + z * z)) * 180.0 / PI;  // Convert radians to degrees
+
+  pitch -= pitch_offset;
+
+  // Print results
+  Serial.print("Pitch: ");
+  Serial.println(pitch);
+
+  if (pitch >= 30 || pitch <= -30) {
+    digitalWrite(LED_PIN, HIGH);
   }
+  else {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  // Run @ ODR 100Hz
+  delay(1000);
+
+  /*jsonString = jsonSerial.readStringUntil('\n');  //Lit le message recu jusqu'a \n
+  jsonErrorMessage = jsonString;
+  jsonError = deserializeJson(doc, jsonString);  //Décode l'objet
 
   if (!jsonError) { //Si le JSON est décodé avec succès
     trottinette.model_num = doc["model_num"];                     //Numéro de la trottinette
@@ -69,10 +129,13 @@ void loop() {
     trottinette.drive.courant_total = doc["drive"]["courant_total"];  //Courant total au moteur
     trottinette.drive.energie = doc["drive"]["energie"];              //Puissance consommée par le moteur
     trottinette.drive.vitesse = doc["drive"]["vitesse"].as<String>(); //Vitesse de la trottinette
+    printData(trottinette); //Affiche les données de la trottinette
   }
+  else {  //Si erreur de décodage
+    Serial.println("json error:");
+    Serial.println(jsonErrorMessage);
+  }*/
 
-  printData(trottinette); //Affiche les données de la trottinette
-  delay(500); //Délai de 500ms
 }
 
 //Nom: printData
@@ -80,20 +143,20 @@ void loop() {
 //Return: aucun
 //Brief: Affiche les valeurs contenues dans la struct de type Data
 void printData(const Data &t) {
-  Serial.println("==== Trottinette Data ====");
+  Serial.println("==== Trottinette Data ====\n");
   Serial.print("Model Num: "); Serial.println(t.model_num);
 
-  Serial.println("-- Ctrl --");
+  Serial.print("-- Ctrl --\n");
   Serial.print("Etat: "); Serial.println(t.ctrl.etat);
   Serial.print("Break Active: "); Serial.println(t.ctrl.break_active);
   Serial.print("Fault Code: "); Serial.println(t.ctrl.fault_code);
   Serial.print("Cmd: "); Serial.println(t.ctrl.cmd);
 
-  Serial.println("-- Drive --");
+  Serial.print("-- Drive --\n");
   Serial.print("Temperature: "); Serial.println(t.drive.temperature);
   Serial.print("Tension Total: "); Serial.println(t.drive.tension_total);
   Serial.print("Courant Total: "); Serial.println(t.drive.courant_total);
   Serial.print("Energie: "); Serial.println(t.drive.energie);
   Serial.print("Vitesse: "); Serial.println(t.drive.vitesse);
-  Serial.println("==========================\n\n");
+  Serial.print("==========================\n\n");
 }
